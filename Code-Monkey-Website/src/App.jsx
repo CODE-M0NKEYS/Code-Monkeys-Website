fd8b77444a29813a1234476a9ed5b45040621667
Reph_193 <vingvolten@gmail.com>
2026-04-20 10:45:13 +0200

    rough start

--- Code-Monkey-Website/src/App.jsx
+++ Code-Monkey-Website/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Navbar from "./components/Navbar";
+import Home from "src/pages/Home";
+import About from "src/pages/About";
+import Contact from "src/pages/Contact";
+import Navbar from "src/components/Navbar";
 
 function App() {
   const [theme, setTheme] = useState("light");
